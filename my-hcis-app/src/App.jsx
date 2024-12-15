8f10e46658e2b128d4fe6269a3029cb956ba628b
M7mdddibrahim <m7mdalhussini@gmail.com>
2024-12-16 00:37:52 +0200

    editmodal 90%, appointment done

--- my-hcis-app/src/App.jsx
+++ my-hcis-app/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import "./index.css";
-
 import D_ProfilePage from "./Components/D_ProfilePage1.jsx/D_ProfilePage";
+import D_PatientList from "./Components/D_ProfilePage1.jsx/D_PatientList/D_PatientList";
 
 const App = () => {
   return (
     <div>
       <D_ProfilePage />
+      
     </div>
   );
 };
