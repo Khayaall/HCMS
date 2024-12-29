820325796064ab60c20238ee0a885f9fcf78d7cb
shehap <115353206+shehab400@users.noreply.github.com>
2024-12-29 22:25:13 +0200

    succesfuly deployed db and connected the db to the backend

--- Backend/db_connection.js
+++ Backend/db_connection.js
@@ -1,12 +1,31 @@
 const { Pool } = require('pg');
+require('dotenv').config();
+
+// const pool = new Pool({
+//     user: 'postgres',          // Replace with your PostgreSQL username
+//     host: 'localhost',         // Database host
+//     database: 'postgres',  // Database name
+//     password: 'admin',  // Ensure this is a string
+//     port: 5432,                // Default PostgreSQL port
+//   });
+
+// pool.connect()
+//   .then(client => {
+//     console.log('Connected to the database');
+//     client.release();
+//   })
+//   .catch(err => {
+//     console.error('Database connection error:', err.stack);
+//   });
+// Use a connection pool for better connection management
+
 
 const pool = new Pool({
-    user: 'postgres',          // Replace with your PostgreSQL username
-    host: 'localhost',         // Database host
-    database: 'postgres',  // Database name
-    password: 'admin',  // Ensure this is a string
-    port: 5432,                // Default PostgreSQL port
-  });
+  connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 pool.connect()
   .then(client => {
