addfec2fde7c70148d0d6178f24710f6ee124709
Ziad Elnady <ziyadahmedza778@gmail.com>
2024-12-30 21:47:45 +0200

    fixing apis

--- front/HCMS-Ziyad/my-hcis-app/src/Pages/D_patientDetails/D_patientDetails.jsx
+++ front/HCMS-Ziyad/my-hcis-app/src/Pages/D_patientDetails/D_patientDetails.jsx
@@ -6,7 +6,7 @@ import PatientDetailsTable from "../../Components/PatientDetails/PatientDetailsT
 const D_patientDetails = () => {
   const { patientId } = useParams();
   const [patient, setPatient] = useState({});
-  const [cancer, setCancer] = useState({});
+  const [cancerData, setCancerData] = useState({});
 
   const token = localStorage.getItem("token");
   const id = localStorage.getItem("id");
@@ -49,12 +49,13 @@ const D_patientDetails = () => {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
-            session_date: "2024-10-10",
-            cancer_stage: "stage 2",
-            dosage: "60mg",
+            session_date: "2024-12-30",
+            cancer_stage: "1",
+            dosage: "40mg",
             age: "45",
-            blood_pressure: "120/90",
-            heart_rate: "76",
+            blood_pressure: "120/80",
+            heart_rate: "70",
+            treatment_type: "Follow-up Care",
           }),
         }
       );
@@ -67,14 +68,70 @@ const D_patientDetails = () => {
       throw new Error("Failed to fetch cancer details");
     }
   };
-  // const zby = {
-  //   session_date,
-  //   cancer_stage,
+  const fetchCancerData = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:5000/doctor/get_cancer_treatment_plan/${patientId}`,
+        {
+          method: "GET",
+          headers: {
+            authorization: `Bearer ${token}`,
+            "User-Id": id,
+            "User-Role": role,
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch cancerrrr");
+      }
+      const cancerData = await response.json();
+      setCancerData(cancerData);
+      console.log(cancerData);
+    } catch (error) {
+      console.error("Error fetching cancer data:", error);
+      throw new Error("Failed to fetch cancer details");
+    }
+  };
+  // const fetchEditCancer = async () => {
+  //   try {const resp = await fetch(
+  //     `http://localhost:5000/doctor/edit_treatment_plan/${patientId}`,
+  //     {
+  //       method: "PUT",
+  //       headers: {
+  //         authorization: `Bearer ${token}`,
+  //         "User-Id": id,
+  //         "User-Role": role,
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({
+  //         session_date: "2024-12-30",
+  //         cancer_stage: "1",
+  //         dosage: "40mg",
+  //         age: "45",
+  //         blood_pressure: "120/80",
+  //         heart_rate: "70",
+  //         treatment_type: "Follow-up Care",
+  //       }),
+  //     }
+  //   );
+  //   console.log(resp);
+  //   if (!resp.ok) {
+  //     throw new Error("Failed to fetch cancer details");
+  //   }
+  // } catch (error) {
+  //   console.error("Error fetching cancer:", error);
+  //   throw new Error("Failed to fetch cancer details");
+  // }
+  // }
 
   useEffect(() => {
     fetchPatient();
-    fetchCancer();
+    // fetchCancer();
+    fetchCancerData();
   }, [patientId]);
+  if (!patient) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="D-patientDetails">
