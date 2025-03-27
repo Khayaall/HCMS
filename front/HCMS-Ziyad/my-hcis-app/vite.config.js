419e54e2ae956d5a2bd4387a5f35c92fcaebb914
Khayaall <ziyadahmedza778@gmail.com>
2025-03-27 16:43:10 +0200

    Add allowedHosts configuration

--- front/HCMS-Ziyad/my-hcis-app/vite.config.js
+++ front/HCMS-Ziyad/my-hcis-app/vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["sha8alny.uaenorth.cloudapp.azure.com"],
+  },
 });
